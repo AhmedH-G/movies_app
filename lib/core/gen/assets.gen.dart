e2f120c2387711b3956ba6a8dad1984dc1b64846
AmnaSalah2002 <amnasalah306@gmail.com>
2026-03-15 02:00:11 +0200

    adding new files

---
+++ lib/core/gen/assets.gen.dart
@@ -0,0 +1,439 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Folder.svg
+  SvgGenImage get folder => const SvgGenImage('assets/icons/Folder.svg');
+
+  /// File path: assets/icons/Group 5.png
+  AssetGenImage get group5 => const AssetGenImage('assets/icons/Group 5.png');
+
+  /// File path: assets/icons/Profiel.png
+  AssetGenImage get profiel => const AssetGenImage('assets/icons/Profiel.png');
+
+  /// File path: assets/icons/User-4.png
+  AssetGenImage get user4 => const AssetGenImage('assets/icons/User-4.png');
+
+  /// File path: assets/icons/Vector (3).png
+  AssetGenImage get vector3 =>
+      const AssetGenImage('assets/icons/Vector (3).png');
+
+  /// File path: assets/icons/Vector (4).png
+  AssetGenImage get vector4 =>
+      const AssetGenImage('assets/icons/Vector (4).png');
+
+  /// File path: assets/icons/Vector (5).png
+  AssetGenImage get vector5 =>
+      const AssetGenImage('assets/icons/Vector (5).png');
+
+  /// File path: assets/icons/explore.png
+  AssetGenImage get explore => const AssetGenImage('assets/icons/explore.png');
+
+  /// File path: assets/icons/eye-slash.png
+  AssetGenImage get eyeSlash =>
+      const AssetGenImage('assets/icons/eye-slash.png');
+
+  /// File path: assets/icons/google.png
+  AssetGenImage get google => const AssetGenImage('assets/icons/google.png');
+
+  /// File path: assets/icons/likes.svg
+  SvgGenImage get likes => const SvgGenImage('assets/icons/likes.svg');
+
+  /// File path: assets/icons/list.svg
+  SvgGenImage get list => const SvgGenImage('assets/icons/list.svg');
+
+  /// File path: assets/icons/lock.png
+  AssetGenImage get lock => const AssetGenImage('assets/icons/lock.png');
+
+  /// File path: assets/icons/phone.png
+  AssetGenImage get phone => const AssetGenImage('assets/icons/phone.png');
+
+  /// File path: assets/icons/rate.svg
+  SvgGenImage get rate => const SvgGenImage('assets/icons/rate.svg');
+
+  /// File path: assets/icons/sms.png
+  AssetGenImage get sms => const AssetGenImage('assets/icons/sms.png');
+
+  /// File path: assets/icons/time.svg
+  SvgGenImage get time => const SvgGenImage('assets/icons/time.svg');
+
+  /// File path: assets/icons/user.png
+  AssetGenImage get user => const AssetGenImage('assets/icons/user.png');
+
+  /// List of all assets
+  List<dynamic> get values => [
+    folder,
+    group5,
+    profiel,
+    user4,
+    vector3,
+    vector4,
+    vector5,
+    explore,
+    eyeSlash,
+    google,
+    likes,
+    list,
+    lock,
+    phone,
+    rate,
+    sms,
+    time,
+    user,
+  ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/Available Now.png
+  AssetGenImage get availableNow =>
+      const AssetGenImage('assets/images/Available Now.png');
+
+  /// File path: assets/images/Component 11 – 1.png
+  AssetGenImage get component111 =>
+      const AssetGenImage('assets/images/Component 11 – 1.png');
+
+  /// File path: assets/images/Component 11 – 10.png
+  AssetGenImage get component1110 =>
+      const AssetGenImage('assets/images/Component 11 – 10.png');
+
+  /// File path: assets/images/Component 11 – 2.png
+  AssetGenImage get component112 =>
+      const AssetGenImage('assets/images/Component 11 – 2.png');
+
+  /// File path: assets/images/Component 11 – 4.png
+  AssetGenImage get component114 =>
+      const AssetGenImage('assets/images/Component 11 – 4.png');
+
+  /// File path: assets/images/Component 11 – 5.png
+  AssetGenImage get component115 =>
+      const AssetGenImage('assets/images/Component 11 – 5.png');
+
+  /// File path: assets/images/Component 11 – 6.png
+  AssetGenImage get component116 =>
+      const AssetGenImage('assets/images/Component 11 – 6.png');
+
+  /// File path: assets/images/Component 11 – 7.png
+  AssetGenImage get component117 =>
+      const AssetGenImage('assets/images/Component 11 – 7.png');
+
+  /// File path: assets/images/Component 11 – 8.png
+  AssetGenImage get component118 =>
+      const AssetGenImage('assets/images/Component 11 – 8.png');
+
+  /// File path: assets/images/Component 11 – 9.png
+  AssetGenImage get component119 =>
+      const AssetGenImage('assets/images/Component 11 – 9.png');
+
+  /// File path: assets/images/EG.png
+  AssetGenImage get eg => const AssetGenImage('assets/images/EG.png');
+
+  /// File path: assets/images/LR.png
+  AssetGenImage get lr => const AssetGenImage('assets/images/LR.png');
+
+  /// File path: assets/images/Logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/images/Logo.png');
+
+  /// File path: assets/images/Watch Now.png
+  AssetGenImage get watchNow =>
+      const AssetGenImage('assets/images/Watch Now.png');
+
+  /// File path: assets/images/avatar1.png
+  AssetGenImage get avatar1 => const AssetGenImage('assets/images/avatar1.png');
+
+  /// File path: assets/images/avatar2.png
+  AssetGenImage get avatar2 => const AssetGenImage('assets/images/avatar2.png');
+
+  /// File path: assets/images/avatar3.png
+  AssetGenImage get avatar3 => const AssetGenImage('assets/images/avatar3.png');
+
+  /// File path: assets/images/avatar4.png
+  AssetGenImage get avatar4 => const AssetGenImage('assets/images/avatar4.png');
+
+  /// File path: assets/images/avatar5.png
+  AssetGenImage get avatar5 => const AssetGenImage('assets/images/avatar5.png');
+
+  /// File path: assets/images/avatar6.png
+  AssetGenImage get avatar6 => const AssetGenImage('assets/images/avatar6.png');
+
+  /// File path: assets/images/avatar7.png
+  AssetGenImage get avatar7 => const AssetGenImage('assets/images/avatar7.png');
+
+  /// File path: assets/images/avatar8.png
+  AssetGenImage get avatar8 => const AssetGenImage('assets/images/avatar8.png');
+
+  /// File path: assets/images/avatar9.png
+  AssetGenImage get avatar9 => const AssetGenImage('assets/images/avatar9.png');
+
+  /// File path: assets/images/background.png
+  AssetGenImage get background =>
+      const AssetGenImage('assets/images/background.png');
+
+  /// File path: assets/images/forget_password.png
+  AssetGenImage get forgetPassword =>
+      const AssetGenImage('assets/images/forget_password.png');
+
+  /// File path: assets/images/gamer (1).png
+  AssetGenImage get gamer1 =>
+      const AssetGenImage('assets/images/gamer (1).png');
+
+  /// File path: assets/images/onBoarding1.png
+  AssetGenImage get onBoarding1 =>
+      const AssetGenImage('assets/images/onBoarding1.png');
+
+  /// File path: assets/images/onBoarding2.jpg
+  AssetGenImage get onBoarding2 =>
+      const AssetGenImage('assets/images/onBoarding2.jpg');
+
+  /// File path: assets/images/onBoarding3.jpg
+  AssetGenImage get onBoarding3 =>
+      const AssetGenImage('assets/images/onBoarding3.jpg');
+
+  /// File path: assets/images/onBoarding4.jpg
+  AssetGenImage get onBoarding4 =>
+      const AssetGenImage('assets/images/onBoarding4.jpg');
+
+  /// File path: assets/images/onBoarding5.jpg
+  AssetGenImage get onBoarding5 =>
+      const AssetGenImage('assets/images/onBoarding5.jpg');
+
+  /// File path: assets/images/onBoarding6.jpg
+  AssetGenImage get onBoarding6 =>
+      const AssetGenImage('assets/images/onBoarding6.jpg');
+
+  /// File path: assets/images/splash_logo.png
+  AssetGenImage get splashLogo =>
+      const AssetGenImage('assets/images/splash_logo.png');
+
+  /// File path: assets/images/video play.png
+  AssetGenImage get videoPlay =>
+      const AssetGenImage('assets/images/video play.png');
+
+  /// File path: assets/images/popcorn.png
+  AssetGenImage get popcorn =>
+      const AssetGenImage('assets/images/popcorn.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+    availableNow,
+    component111,
+    component1110,
+    component112,
+    component114,
+    component115,
+    component116,
+    component117,
+    component118,
+    component119,
+    eg,
+    lr,
+    logo,
+    watchNow,
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatar5,
+    avatar6,
+    avatar7,
+    avatar8,
+    avatar9,
+    background,
+    forgetPassword,
+    gamer1,
+    onBoarding1,
+    onBoarding2,
+    onBoarding3,
+    onBoarding4,
+    onBoarding5,
+    onBoarding6,
+    splashLogo,
+    videoPlay,
+    popcorn
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
+
+class SvgGenImage {
+  const SvgGenImage(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = false;
+
+  const SvgGenImage.vec(this._assetName, {this.size, this.flavors = const {}})
+    : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    _svg.ColorMapper? colorMapper,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+        colorMapper: colorMapper,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter:
+          colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
